8b236b069855b7d8a182f9072624d7bda1384dae
Philip Kalinsky <philip.kalinsky@eloquentbits.com>
2025-07-10 16:31:38 -0400

    set STATIC_ROOT

--- eweb/core/settings.py
+++ eweb/core/settings.py
@@ -136,8 +136,10 @@ MEDIA_URL = "/media/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+#
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
